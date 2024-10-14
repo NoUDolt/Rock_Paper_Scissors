bdae4e6975c668789d1e48f920031f9bcdc4b1c4
NoUDolt <48100454+NoUDolt@users.noreply.github.com>
2024-10-14 17:47:51 -0400

    cleaned up code

--- src/RockPaperScissorsFrame.java
+++ src/RockPaperScissorsFrame.java
@@ -1,7 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
-import java.util.ArrayList;
 import java.util.Random;
 
 public class RockPaperScissorsFrame extends JFrame {
